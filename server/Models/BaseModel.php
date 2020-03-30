e17f02d19aad58d56f36c181127b83f2a81a21d3
Tymek Pisko <tymek@simplycolors.nl>
2020-03-30 15:19:07 +0200

    start of api

--- server/Models/BaseModel.php
+++ server/Models/BaseModel.php
@@ -1,14 +1,59 @@
 <?php
 
+include_once "../Database/DatabaseConnection.php";
 
-class BaseModel extends DatabaseConnection
+class BaseModel
 {
-    protected function create(){
+    public $tableName;
 
+    protected function create($values){
+
+        $query = "INSERT INTO " . $this->tableName . " SET ";
+
+        $i = 0;
+        foreach($values as $key => $value){
+            if($i === 0){
+                $query .= $key . "=:" . $i;
+            }else{
+                $query .= ", ". $key . "=:" . $i;
+            }
+            $i++;
+        }
+
+        $database = new DatabaseConnection();
+        $database->getConnection();
+        $stmt = $database->conn->prepare($query);
+
+        array_map('changeValue', $values);
+        function changeValue($val){
+            htmlspecialchars(strip_tags($val));
+        }
+
+        $i = 0;
+        foreach($values as $key => &$value){
+            $index = ':'.$i;
+            $stmt->bindParam($index,$value);
+            $i++;
+        }
+        if($stmt->execute()){
+            return true;
+        }
+        return false;
     }
 
-    protected function read(){
+    public function read($key ,$where){
+        if($where == 0){
+            $query = "SELECT * FROM " . $this->tableName;
+        }else{
+            $query = "SELECT * FROM" . $this->tableName . "WHERE " . $key . "=" . $where;
+        }
+        $database = new DatabaseConnection();
+        $database->getConnection();
 
+        $stmt = $database->conn->prepare($query);
+        $stmt->execute();
+
+        return $stmt;
     }
 
     protected function update(){
@@ -18,5 +63,5 @@ class BaseModel extends DatabaseConnection
     protected function delete(){
 
     }
-
 }
+?>
