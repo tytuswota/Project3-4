9c2a6ee94f5ddd2d77b8bf04912bd136237598cd
Jaco0987752 <0987752@hr.nl>
2020-04-02 15:29:24 +0200

    Password box updated on key event

--- client/gui/applicatie/src/main/java/org/openjfx/loginController.java
+++ client/gui/applicatie/src/main/java/org/openjfx/loginController.java
@@ -2,11 +2,16 @@ package org.openjfx;
 
 import java.io.IOException;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import org.openjfx.loginController;
 
 public class loginController {
 
+    @FXML
+    PasswordField pin;
     @FXML
     Button bevestigen;
     @FXML
@@ -22,5 +27,17 @@ public class loginController {
     public void switchToPasUit() throws IOException {
         App.setRoot("pasUit");
     }
+
+    SerialReader serialReader = new SerialReader() {
+        @Override
+        protected void KeyPressEvent(String key) {
+            Platform.runLater(new Runnable() {
+                @Override
+                public void run() {
+                    pin.appendText(key);
+                }
+            });
+        }
+    };
 
 }
