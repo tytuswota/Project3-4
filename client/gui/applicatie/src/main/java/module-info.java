5d0d268016d4692337b4ef2a6f281d631fd6f8cb
Jaco0987752 <0987752@hr.nl>
2020-04-03 14:36:12 +0200

    Resolving required error.

--- client/gui/applicatie/src/main/java/module-info.java
+++ client/gui/applicatie/src/main/java/module-info.java
@@ -4,6 +4,8 @@
 module org.openjfx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.fazecast.jSerialComm;
+    requires org.json;
 
     opens org.openjfx to javafx.fxml;
     exports org.openjfx;
