84dd804c244a180fe36f07849eda6211e091c117
Jaco0987752 <0987752@hr.nl>
2020-04-03 14:07:04 +0200

    Changing test-program, keypad is working.

--- client/arduino test/test-programma-serial/test-programma-serial.ino
+++ client/arduino test/test-programma-serial/test-programma-serial.ino
@@ -1,8 +1,34 @@
+#include <Keypad.h>
+/**
+ * testprogram for the keypad communication.
+ * 
+ */
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //three columnsa
+char keys[ROWS][COLS] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+
+// Initializing pins for keypad
+byte rowPins[ROWS] = {9, 8, 7, 6}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {5, 4, 3, 2}; //connect to the column pinouts of the keypad
+
+// Create instance for keypad
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.println("{\"keypress\":\"1\"}");
-  delay(1000);
+  char key = keypad.getKey();
+  if(key){// sent only when a key is present
+      Serial.print("{\"keypress\":\"");
+      Serial.print(key);
+      Serial.println("\"}");
+  }
 }
