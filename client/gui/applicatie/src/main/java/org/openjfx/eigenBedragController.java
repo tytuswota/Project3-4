c1f776b2206a8584f0326e0d3422acda037d3fd6
Jaco0987752 <0987752@hr.nl>
2020-04-06 15:32:31 +0200

    Connection between eigenBedragController and serial

--- client/gui/applicatie/src/main/java/org/openjfx/eigenBedragController.java
+++ client/gui/applicatie/src/main/java/org/openjfx/eigenBedragController.java
@@ -1,19 +1,41 @@
 package org.openjfx;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 
 public class eigenBedragController {
 
+    SerialReader reader;
+
+    public eigenBedragController() {
+        reader = SerialReader.GetReader();
+        reader.addKeyPadListener((x) -> {
+            KeyPressEventHandler(x);
+        });
+    }
+
     @FXML
     Button eigenBedragToMenu;
+
+    @FXML
+    TextField saldoText;
 
     @FXML
     public void switchToMainMenu() throws IOException {
         App.setRoot("mainMenu");
     }
 
+    public void KeyPressEventHandler(String key){
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                saldoText.appendText(key);
+            }
+        });
+    }
 
 }
