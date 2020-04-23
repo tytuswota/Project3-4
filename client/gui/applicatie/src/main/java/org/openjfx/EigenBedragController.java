88cf9fc20c9b40fa203c35cc73742ff8e4b49782
Jaco0987752 <0987752@hr.nl>
2020-04-23 20:01:48 +0200

    Abstracting some code.

--- client/gui/applicatie/src/main/java/org/openjfx/EigenBedragController.java
+++ client/gui/applicatie/src/main/java/org/openjfx/EigenBedragController.java
@@ -1,79 +1,24 @@
 package org.openjfx;
 
-import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-
 import javafx.scene.control.TextField;
-import model.SerialReader;
-
 import java.io.IOException;
 
-public class EigenBedragController {
-
-    SerialReader reader;
-
-    public EigenBedragController() {
-        reader = SerialReader.GetReader();
-        reader.addKeyPadListener((x) -> {
-            KeyPressEventHandler(x);
-        });
-    }
+public class EigenBedragController extends BaseController {
 
     @FXML
     Button eigenBedragToMenu;
 
     @FXML
-
     TextField saldoText;
+
     @FXML
     public void switchToMainMenu() throws IOException {
         App.setRoot("mainMenu");
     }
 
-    public void KeyPressEventHandler(String key){
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                saldoText.appendText(key);
-            }
-        });
+    public void KeyPressEventHandler(String key) {
+        saldoText.appendText(key);
     }
-
-//    SerialReader serialReader = new SerialReader() {
-//        @Override
-//        protected void qwerty(String key) {
-//            Platform.runLater(new Runnable() {
-//                @Override
-//                public void run() {
-//
-////                    if (key == "#") {
-////                    }
-//
-//                    char car = key.charAt(0);
-//
-//                    if ((car >= '0' && car <= '9')) {
-//
-//                        eigenBedrag.appendText(key);
-//                        System.out.println(key);
-//                    }
-//                }
-//            });
-//        }
-//    };
-
-
-//    SerialReader serialReader = new SerialReader() {
-//        @Override
-//        protected void KeyPressEvent(String key) {
-//            Platform.runLater(new Runnable() {
-//                @Override
-//                public void run() {
-//
-//                        eigenBedrag.appendText(key);
-//                }
-//            });
-//        }
-//    };
 }
-
