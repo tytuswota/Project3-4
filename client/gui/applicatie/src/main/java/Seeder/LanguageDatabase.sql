0ed6fbec541d2c25d19ae2adbc64e413a1472a16
Jaco0987752 <0987752@hr.nl>
2020-05-05 12:13:10 +0200

    Adding database script

---
+++ client/gui/applicatie/src/main/java/Seeder/LanguageDatabase.sql
@@ -0,0 +1,18 @@
+create table translations(id VARCHAR(), nederlands VARCHAR(), english VARCHAR());
+insert into translations values 
+("other","other","anders"),
+("menu","menu","menu"),
+("goBack","druk # om terug te gaan","type # to return"),
+("desiredAmount","voer het gewenst bedrag in","enter the desired amount"),
+("backspace","backspace","backspace"),
+("confirm","bevestigen","confirm"),
+("balance","saldo",balance),
+("quit","afbreken","quit"),
+("withdraw","pinnen","withdraw"),
+("withdraw70","€ 70 pinnen","€ 70 withdraw"),
+("cardOut","neem uw pas uit","take your card out"),
+("cardIn","voer uw pas in","take your card out"),
+("noConnection","Sorry, geen verbinding",""),
+("mainMenu", "hooftmenu","mainmenu"),
+("yourBalance","uw saldo is:","your balance is:"),
+("enterPin", "voer uw pincode in", "enter your pin")
