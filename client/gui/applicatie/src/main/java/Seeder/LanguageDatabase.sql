702b9e36ce976fc9b1fd692f5496cab34a2ed274
Jaco0987752 <0987752@hr.nl>
2020-05-05 14:35:53 +0200

    Adding russian

--- client/gui/applicatie/src/main/java/Seeder/LanguageDatabase.sql
+++ client/gui/applicatie/src/main/java/Seeder/LanguageDatabase.sql
@@ -1,18 +1,18 @@
-create table translations(id VARCHAR(), nederlands VARCHAR(), english VARCHAR());
+create table Translations(id VARCHAR(100), nederlands VARCHAR(100), english VARCHAR(100), russian VARCHAR(100));
 insert into translations values 
-("other","other","anders"),
-("menu","menu","menu"),
+("other","anders","other","другой"),
+("menu","menu","menu","меню"),
 ("goBack","druk # om terug te gaan","type # to return"),
-("desiredAmount","voer het gewenst bedrag in","enter the desired amount"),
-("backspace","backspace","backspace"),
-("confirm","bevestigen","confirm"),
-("balance","saldo",balance),
-("quit","afbreken","quit"),
+("desiredAmount","voer het gewenst bedrag in","enter the desired amount","введите # для возврата"),
+("backspace","backspace","backspace","бекспейс"),
+("confirm","bevestigen","confirm","подтверждения"),
+("balance","saldo","balance", "баланс"),
+("quit","afbreken","quit", "прекращать"),
 ("withdraw","pinnen","withdraw"),
-("withdraw70","€ 70 pinnen","€ 70 withdraw"),
-("cardOut","neem uw pas uit","take your card out"),
-("cardIn","voer uw pas in","take your card out"),
-("noConnection","Sorry, geen verbinding",""),
-("mainMenu", "hooftmenu","mainmenu"),
+("withdraw70","€ 70 pinnen","€ 70 withdraw", "изымать"),
+("cardOut","neem uw pas uit","take your card out", "вытащи свою карточку"),
+("cardIn","voer uw pas in","insert your card","вставьте свою карту"),
+("noConnection","Sorry, geen verbinding","Sorry, no connection", "Извините, нет связи"),
+("mainMenu", "hooftmenu","mainmenu", "главное меню"),
 ("yourBalance","uw saldo is:","your balance is:"),
-("enterPin", "voer uw pincode in", "enter your pin")
+("enterPin", "voer uw pincode in", "enter your pin", "ваш баланс:") 
