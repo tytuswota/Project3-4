3ba425d65e0dd6e98c9ada89988b3e823487a6f0
tytuswota <wabbijack@gmail.com>
2020-05-06 15:45:08 +0200

    no negative value

--- server/Controllers/TransactionController.php
+++ server/Controllers/TransactionController.php
@@ -14,7 +14,7 @@ class TransactionController extends BaseController
 
     static function withdraw($accountId, $amount){
         $accountBalance = self::getMaxWidthDraw($accountId);
-        if($amount >= 0){
+        if($amount > 0){
             $result = $accountBalance - $amount;
 
             if($result >= 0){
