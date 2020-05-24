92c8d3d2896d77530b1a126418cb5001f6bf0801
shab1r <0985130@hr.nl>
2020-05-24 02:07:09 +0200

    its now much better to see what options you have when withdrawing. done by using multiple labels.

--- client/gui/applicatie/src/main/java/model/BankNoteCombo.java
+++ client/gui/applicatie/src/main/java/model/BankNoteCombo.java
@@ -6,17 +6,17 @@ import java.util.ArrayList;
 public class BankNoteCombo {
     private int amount;
 
-    public int[] calBankNoteCombo(int amount){
+    public int[] calBankNoteCombo(int amount) {
 
 
         this.amount = amount;
         int[] bankNotes = new int[3];
 
-        if(this.amount % 10 == 0 && this.amount <= 100){
+        if (this.amount % 10 == 0 && this.amount <= 60 && this.amount >= 10) {
             bankNotes[0] = 10;
         }
 
-        if(this.amount % 20 == 0 || this.amount % 20 == 10 && this.amount <= 200){
+        if (this.amount % 20 == 0 || this.amount % 20 == 10 && this.amount >= 20 && this.amount <= 200) {
             bankNotes[1] = 20;
         }
 
