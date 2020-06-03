3fb620456bb7235c545493c96b581fec098926f8
robin <robinmorais@hotmail.com>
2020-06-03 15:09:07 +0200

    made a start with receipt printer. (test fase)

--- client/gui/applicatie/src/main/java/org/openjfx/BonController.java
+++ client/gui/applicatie/src/main/java/org/openjfx/BonController.java
@@ -1,7 +1,9 @@
 package org.openjfx;
 
+import com.fazecast.jSerialComm.SerialPort;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -16,14 +18,29 @@ public class BonController extends BaseController {
 
     @FXML
     public void switchToPasUit() throws IOException {
-        App.setRoot("pasUit");
+        App.setRoot("simulatie");
     }
 
     @Override
     public void KeyPressEventHandler(char key) {
         try {
             if (key == '#') {
-                //bon  functie moet komen
+                SerialPort comPort= SerialPort.getCommPort("COM3");
+                comPort.setComPortParameters(19200,8,1,0);
+                comPort.setComPortTimeouts(SerialPort.TIMEOUT_WRITE_BLOCKING,0,0);
+                comPort.openPort();
+
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("amount", BaseController.getTotBedrag());
+                    for(int i=0; i< 3; i++) {
+                        byte[] stringByteArray = jsonObject.toString().getBytes();
+                        String string = new String(stringByteArray);
+                        System.out.println(string);
+                        comPort.writeBytes(stringByteArray, stringByteArray.length);
+                        Thread.sleep(2000);
+                    }
+
+                //todo: logic bonprinter;
                 switchToPasUit();
 
 
